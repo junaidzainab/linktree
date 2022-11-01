3c7a6f596e6a963d9f1cb0e3bd14ea48503908e3
junaid zainab <junaidhorlameedeh@gmail.com>
2022-11-01 14:01:32 +0100

    my finished task

--- src/App.js
+++ src/App.js
@@ -15,11 +15,27 @@ function App() {
         </div>
         <div className="">
             <div className='Mrg'><a href="https://twitter.com/junaidzainab" target="_blank" >Twitter Link</a></div>
-            <div className='Mrg' id="btn-zuri"><a href="https://training.zuri.team/" target="_blank">Zuri Team</a></div>
-            <div className='Mrg' id="book"><a href="http://books.zuri.team" target="_blank">Zuri Book</a></div>
-            <div className='Mrg' id="book_python"><a href="https://books.zuri.team" target="_blank">Python Book</a></div>
-            <div className='Mrg' id="pitch"><a href="https://background.zuri.team" target="_blank">Background Check</a></div>
-            <div className='Mrg' id="book_design"><a href="https://books.zuri.team/design-rules" target="_blank">Design Book</a></div>
+            <div className='Mrg' id="btn-zuri">
+              <a href="https://training.zuri.team/" target="_blank">Zuri Team</a>
+            </div>
+            <div className='Mrg' id="book">
+              <a href="http://books.zuri.team" target="_blank">Zuri Book</a>
+              <p className='Font-13 Clr-txt'>This page is useful and resorceful, it is a place where i
+               find books on coding and design</p>
+              </div>
+            <div className='Mrg' id="book_python">
+              <a href="https://books.zuri.team/python-for-beginners?ref_id=junaidzainab" target="_blank">Python Book</a>
+              <p className='Font-13 Clr-txt'>Getting this book as a biginner will really help and aid your learning faster, 
+              and it will equip you with the neccesary knowledge</p>
+              </div>
+            <div className='Mrg' id="pitch">
+              <a href="https://background.zuri.team" target="_blank">Background Check</a>
+            <p className='Font-13 Clr-txt'>Am Zainab a web developer, am passionate about learning, helping others, growth and development.</p>
+            </div>
+            <div className='Mrg' id="book_design">
+              <a href="https://books.zuri.team/design-rules" target="_blank">Design Book</a>
+            <p className='Font-13 Clr-txt'>Check out this free design book by Zuri, it really worth reading</p>
+               </div>
         </div>
         <div className='Dflex Wit'>
             <div><img src={slack} alt="slack"/></div> 
